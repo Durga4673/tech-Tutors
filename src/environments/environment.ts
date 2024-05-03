8210df3c4d7118219c927b1bdc1821c5b6275489
Durga prasad <durgaprasad.kurada@peopletech.com>
2024-05-03 16:48:13 +0530

    -added changes new changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: false,
+    production: true,
     CONNECTION_URL : 'https://sheet.best/api/sheets/c77e97c0-b97a-497a-b777-c40fabcb90fe',
+
   };
+
+  export const apiUrl = 'https://script.google.com/macros/s/AKfycbwoL7A03UEfLGTLTDoUYwSYLki0JPy3QCvmw7kCOdkEwWf_PM4DW1T_09ufkmcnUHAJ/exec';
